ae57abe88f8d1c22b3fea2e682ba9821dcbf4805
Pierre de Montalte <pierredemontalte@gmail.com>
2024-02-06 16:34:53 +0100

    Update simpler.js

--- simpler.js
+++ simpler.js
@@ -1,13 +1,20 @@
 // Version 3
 
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Looping tension',
+      data: [65, 59, 80, 81, 26, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+    }]
+  };
+
 document.addEventListener('DOMContentLoaded', function () {
     
     // Render the chart v1
     const ctx = document.getElementById('chart').getContext('2d');
     new Chart(ctx, {
         type: 'line',
-        data: {
-            labels: [1, 2, 3, 4, 5], // Use the sorted dates as labels
-            datasets: [28, 31, 29, 33, 37]
-        }});
+        data: data,
     });
