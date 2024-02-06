29cecb51b2e25ff7eae0f6574b9383a83659472c
Pierre de Montalte <pierredemontalte@gmail.com>
2024-02-06 21:55:06 +0100

    Update index-v2.js

--- index-v2.js
+++ index-v2.js
@@ -14,8 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
         maxLength = dataLength;
       }
     });
-
-    console.log(stocks);
   
     // 2. Process each stock and prepare its data
     stocks.forEach(stock => {
@@ -27,14 +25,13 @@ document.addEventListener('DOMContentLoaded', function () {
       const tradingDates = Object.keys(jsonData["Monthly Time Series"]);
       const data = new Array(maxLength).fill(null); // Initialize array with nulls
 
-      console.log(ticker, color);
+      console.log(color);
   
       // Fill in data from the end, assuming data starts from the latest month backwards
       for (let i = 0; i < tradingDates.length; i++) {
         const monthIndex = maxLength - i - 1; // Calculate index based on length
         data[monthIndex] = jsonData["Monthly Time Series"][tradingDates[i]]["4. close"];
       }
-
   
       // Add this stock's data to the array
       stockData.push({
@@ -53,74 +50,22 @@ document.addEventListener('DOMContentLoaded', function () {
     // 3. Render the chart
     const ctx = document.getElementById('chart').getContext('2d');
     new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: stockData
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-              display: true // Ensure Y-axis labels are displayed
-              //title: {display: true,text: 'Price (Close)'}
-            },
-            x: {
-                display: true // Ensure X-axis labels are displayed
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: stockData
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Price (Close)'
             }
-          },
-          animations: {
-            duration: 2000, // Duration of the animation in milliseconds
-            easing: 'linear', // Easing function to use for the animation
-            onProgress: function(animation) {
-                progress = animation.currentStep / animation.numSteps;
-            }
-          },
-          plugins: [{
-            id: 'customClip',
-            beforeDatasetDraw: function(chart, args) {
-                const ctx = chart.ctx;
-                const chartArea = chart.chartArea;
-                const width = chartArea.right - chartArea.left;
-
-                if (args.index === 0) { // Apply only to the first dataset
-                    ctx.save();
-                    // Clip the drawing area for the dataset
-                    ctx.beginPath();
-                    ctx.rect(chartArea.left, chartArea.top, width * progress, chartArea.bottom - chartArea.top);
-                    ctx.clip();
-                }
-            },
-            afterDatasetDraw: function(chart, args) {
-                if (args.index === 0) { // Apply only to the first dataset
-                    chart.ctx.restore();
-                }
-            }
-        }],
-          elements: {
-            line: {
-              tension: 0 // Set to 0 to draw straight lines
-            },
-            point: {
-              radius: function(context) {
-                // Dynamically set the radius of each point
-                var index = context.dataIndex;
-                var size = context.dataset.data.length;
-                var currentAnimationStep = context.chart._animationFrame ? context.chart._animationFrame : 0;
-                var animationStepSize = context.chart.animating ? size / (context.chart.options.animation.duration / 16.66) : size;
-                if (index <= currentAnimationStep / animationStepSize) {
-                  return 3; // Size of the point
-                }
-                return 0; // Hide the point if it's not yet time to display it
-              }
-            }
-          },
-          interaction: {
-            intersect: false,
-            mode: 'nearest'
           }
         }
-      });
-      
-      
-});
+      }
+    });
+  });
+ 
