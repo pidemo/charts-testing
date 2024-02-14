ca9adfdddaa3098cf590922fb9673b1c6e65656c
Pierre de Montalte <pierredemontalte@gmail.com>
2024-02-14 18:20:49 +0100

    Update starter.js

--- starter.js
+++ starter.js
@@ -66,14 +66,14 @@ const config = {
         transitions: {
             show: {
               animations: {
-                x: {from: 0},
-                //y: {from: 0}
+                //x: {from: 0},
+                y: {from: 0}
               }
             },
             hide: {
               animations: {
-                //x: {to: 0},
-                y: {to: 0}
+                x: {to: 0},
+                //y: {to: 0}
               }
             }
         }
