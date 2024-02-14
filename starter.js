820c7198a4b4f5071470b829ba21e762c79d35a8
Pierre de Montalte <pierredemontalte@gmail.com>
2024-02-14 17:03:09 +0100

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,5 +1,49 @@
 const ctx = document.getElementById('myChart');
 
+const day = [
+    { x: Date.parse('2024-02-14'), y: 19 },
+    { x: Date.parse('2024-02-15'), y: 12 },
+    { x: Date.parse('2024-02-16'), y: 15 },
+    { x: Date.parse('2024-02-17'), y: 16 },
+    { x: Date.parse('2024-02-18'), y: 14 },
+    { x: Date.parse('2024-02-19'), y: 11 }
+]
+
+  new Chart(ctx, {
+    type: 'line',
+    data: day,
+    /*{
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+      
+    },*/
+    options: {
+      scales: {
+        x: {
+            type: 'time',
+            time: {
+                unit: 'day'
+            }
+        },
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+  /* 
+  
+  // Starter Bundle
+
+  const ctx = document.getElementById('myChart');
+
   new Chart(ctx, {
     type: 'bar',
     data: {
@@ -18,3 +62,5 @@ const ctx = document.getElementById('myChart');
       }
     }
   });
+
+  */
